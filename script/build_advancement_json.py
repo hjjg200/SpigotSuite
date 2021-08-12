2053b265b10d3aace0135bd5ae065a08a00f6648
anton <anton@mbp1>
2021-08-12 16:31:32 +0900

    Add script

---
+++ script/build_advancement_json.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+import json
+import sys
+from pathlib import Path
+
+def print_usage():
+    sys.exit("Usage: build_advancement_json.py <en.json> <other_lang.json> >> out.json")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print_usage()
+    
+    en_path = Path(sys.argv[1])
+    lc_path = Path(sys.argv[2])
+
+    if en_path.suffix != '.json' or lc_path.suffix != '.json':
+        print_usage()
+
+    en_f = open(en_path,)
+    lc_f = open(lc_path,)
+
+    en_data = json.load(en_f)
+    lc_data = json.load(lc_f)
+
+    out_data = dict()
+
+    fmt_key = "chat.type.advancement.task"
+    out_data[fmt_key] = lc_data[fmt_key]
+
+    for k in en_data:
+        if not k.startswith("advancements"):
+            continue
+        if not k.endswith(".title"):
+            continue
+        
+        adv_title = en_data[k]
+        base_key = k[:-6]
+        desc_key = base_key + ".description"
+
+        obj = {'title': lc_data[k], 'description': en_data[desc_key]}
+        out_data[adv_title] = obj
+        if desc_key in lc_data:
+            out_data[adv_title]['description'] = lc_data[desc_key]
+
+    en_f.close()
+    lc_f.close()
+
+    print(json.dumps(out_data, indent=2))
